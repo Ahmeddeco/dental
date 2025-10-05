f99ecb2bc893a8ba03286c176cc5f05156dbc838
ahmed <ahmeddecodesign@gmail.com>
2025-10-05 17:43:25 +0300

    theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,47 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark, shadcn } from "@clerk/themes";
-import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/Header";
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark, shadcn } from "@clerk/themes"
+import Footer from "@/components/layout/Footer"
+import Header from "@/components/layout/Header"
+import { ThemeProvider } from "@/components/layout/theme-provider"
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
-  title: "Dental - AI Powered Dental Assistant",
-  description:
-    "Get instant dental advice through voce calls with our AI assistant. Available 24/7.",
-};
+	title: "Dental - AI Powered Dental Assistant",
+	description: "Get instant dental advice through voce calls with our AI assistant. Available 24/7.",
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: shadcn,
-      }}
-    >
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
-        >
-          <Header />
-          <main className="min-h-dvh pt-14 container mx-auto px-6">
-            {children}
-          </main>
-          <Footer />
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider
+			appearance={{
+				baseTheme: shadcn,
+			}}
+		>
+			<html lang="en" suppressHydrationWarning>
+				<body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
+					<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+						<Header />
+						<main className="min-h-dvh pt-14 container mx-auto px-6">{children}</main>
+						<Footer />
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
+	)
 }
